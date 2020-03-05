1e4df8ff616a08f216e0fc575ffeec1da3b65004
idemudia2002 <idde19@yahoo.com>
2020-03-05 10:31:25 +0100

    logic

--- empty_window.py
+++ empty_window.py
@@ -2,13 +2,22 @@ import pygame
 
 BLACK = (0, 0, 0,)
 WHITE = (255, 255, 255) 
+RED = (255, 0 , 0)
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+
+offset = 0
 
 pygame.init()
 
 size = (800, 500)
 screen = pygame.display.set_mode(size)
 
+pygame.display.set_caption("Dash n Jump")
+
 clock = pygame.time.Clock()
+
+
 
 done = False
 
@@ -16,11 +25,25 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+    
+    #Game logic
+    offset += 1
 
-screen.fill(BLACK)
+    if (offset > 790):
+        offset = 790
+        
+     
+    
 
-pygame.display.flip()
+    screen.fill(BLACK)
+     
+    pygame.draw.line(screen, WHITE, [0, 450], [3000, 450], 10)
+    pygame.draw.rect(screen, RED, [0 + offset, 425, 20, 25], 5)
+    pygame.draw.ellipse(screen, BLUE, [0 + offset, 425, 20, 25], 5)
+    pygame.display.flip()
 
-clock.tick(60)
+    clock.tick(60)
+
 
 pygame.quit()
+

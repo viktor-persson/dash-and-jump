ec490055b1828283bbbe5c3f1af881878c990627
viktor persson <viktor.persson.1@skola.botkyrka.se>
2020-03-09 19:26:45 +0100

    jumping

--- empty_window.py
+++ empty_window.py
@@ -6,6 +6,9 @@ RED = (255, 0 , 0)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 
+
+
+
 offset = 0
 
 pygame.init()
@@ -13,9 +16,18 @@ pygame.init()
 size = (800, 500)
 screen = pygame.display.set_mode(size)
 
+x = 0 + offset
+y = 425
+width = 20
+height = 25
+
+
 pygame.display.set_caption("Dash n Jump")
 
 clock = pygame.time.Clock()
+
+isJump = False
+jumpCount = 10
 
 
 
@@ -31,15 +43,26 @@ while not done:
 
     if (offset > 790):
         offset = 790
-        
 
-    
+    keys = pygame.key.get_pressed()
+
+    if not(isJump):
+        if keys[pygame.K_SPACE]:
+            isJump = True
+    else:
+        if jumpCount >= -10:
+            y -= (jumpCount * abs(jumpCount)) * 0.5
+            jumpCount -= 1
+        else: 
+            jumpCount = 10
+            isJump = False
+
 
     screen.fill(BLACK)
      
     pygame.draw.line(screen, WHITE, [0, 450], [3000, 450], 10)
-    pygame.draw.rect(screen, RED, [0 + offset, 425, 20, 25], 5)
-    pygame.draw.ellipse(screen, BLUE, [0 + offset, 425, 20, 25], 5)
+    pygame.draw.rect(screen, RED, [0 + offset, y, 20, 25], 5) 
+    pygame.draw.ellipse(screen, BLUE, [0 + offset, y, 20, 25], 5)
     pygame.display.flip()
 
     clock.tick(60)

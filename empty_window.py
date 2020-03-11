f1a0cb5dad62802a99ac82e07a1072a6ffe1b754
idemudia2002 <idde19@yahoo.com>
2020-03-11 19:11:15 +0100

    obstacle

--- empty_window.py
+++ empty_window.py
@@ -5,28 +5,20 @@ WHITE = (255, 255, 255)
 RED = (255, 0 , 0)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
-<<<<<<< HEAD
+
 ORANGE = (255,100,10)
-=======
-
-
-
-
->>>>>>> 290031279541ae11225267dca8584b06361a07e4
 offset = 0
-
-o_1 = 700
-o_2 = 720
+obstacles_org = [700, 750, 900, 1000, 1050, 1200, 1300, 1350, 1500, 1600, 1700, 1750, 1850]
 
 
 speed = 100
 pygame.init()
-
+obstacles = obstacles_org.copy()
 size = (800, 500)
 screen = pygame.display.set_mode(size)
 
 x = 0 + offset
-y = 425
+y = 420
 width = 20
 height = 25
 
@@ -37,10 +29,11 @@ clock = pygame.time.Clock()
 
 isJump = False
 jumpCount = 10
-
-
-
 done = False
+
+def draw_obstacle(x_coord):
+    pygame.draw.polygon(screen, ORANGE, [[x_coord, 400], [x_coord -25, 445], [x_coord + 25, 445]])    
+
 
 while not done:
     for event in pygame.event.get():
@@ -48,29 +41,6 @@ while not done:
             done = True
     
     #Game logic
-    offset += 1
-    o1 = o_1 - offset
-    o2 = o_2 - offset
-
-<<<<<<< HEAD
-    if (offset > 700):
-        offset = 0
-           
-
-    
-    #Draw
-    screen.fill(BLACK)
-     
-    pygame.draw.line(screen, WHITE, [0, 450], [3000, 450], 10)
-    pygame.draw.rect(screen, RED, [350, 420, 20, 25], 5)
-    pygame.draw.ellipse(screen, BLUE, [350, 420, 20, 25], 5)
-    
-    pygame.draw.polygon(screen, ORANGE,[[o1, 400], [o1 -10, 445], [o1 +10, 445]])
-    pygame.draw.polygon(screen, ORANGE,[[o2, 400], [o2 -10, 445], [o2 +10, 445]])
-=======
-    if (offset > 790):
-        offset = 790
-
     keys = pygame.key.get_pressed()
 
     if not(isJump):
@@ -84,13 +54,27 @@ while not done:
             jumpCount = 10
             isJump = False
 
-
+    for i in range(len(obstacles)):
+        obstacles[i] -= 1
+        if obstacles[i] < 0:
+            obstacles[i] += 1300
+        
+    
+    #Draw
     screen.fill(BLACK)
      
     pygame.draw.line(screen, WHITE, [0, 450], [3000, 450], 10)
-    pygame.draw.rect(screen, RED, [0 + offset, y, 20, 25], 5) 
-    pygame.draw.ellipse(screen, BLUE, [0 + offset, y, 20, 25], 5)
->>>>>>> 290031279541ae11225267dca8584b06361a07e4
+    pygame.draw.rect(screen, RED, [350, y, 20, 25], 5)
+    pygame.draw.ellipse(screen, BLUE, [350, y, 20, 25], 5)
+    
+#    pygame.draw.polygon(screen, ORANGE,[[o1, 400], [o1 -10, 445], [o1 +10, 445]])
+#    pygame.draw.polygon(screen, ORANGE,[[o2, 400], [o2 -10, 445], [o2 +10, 445]])
+
+    for i in range(len(obstacles)):
+        draw_obstacle(obstacles[i])              
+  
+     
+
     pygame.display.flip()
     
     clock.tick(speed)

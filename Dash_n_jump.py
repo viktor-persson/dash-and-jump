392092ab101d1d45036d37990aa04f13c28c64a9
idemudia2002 <idde19@yahoo.com>
2020-04-27 18:18:45 +0200

    added dash

--- Dash_n_jump.py
+++ Dash_n_jump.py
@@ -1,5 +1,6 @@
 import pygame
 import time
+import copy
 BLACK = (0, 0, 0,)
 WHITE = (255, 255, 255) 
 RED = (255, 0 , 0)
@@ -8,16 +9,19 @@ GREEN = (0, 255, 0)
 ORANGE = (255,100,10)
 
 background = BLACK
+ob_h = 325
+ob_l = 345
+
 
 offset = 0
-obstacles_org = [700, 750, 900, 1000, 1050, 1200, 1300, 1350, 1500, 1650, 1700, 1700, 1850, 1900]
-
+obstacles_org = [[700, ob_l], [750, ob_l], [900,ob_h], [1000,ob_l], [1050,ob_l], [1200,ob_h], [1300,ob_l], [1350,ob_l], [1500,ob_l], [1650,ob_l], [1700,ob_l] , [1850,ob_h], [1900,ob_h]]
+obstacles = []
 coldis = 30
 
 score = 0
 speed = 100
 pygame.init()
-obstacles = obstacles_org.copy()
+obstacles = copy.deepcopy(obstacles_org)
 size = (800, 500)
 screen = pygame.display.set_mode(size)
 
@@ -30,8 +34,10 @@ pygame.display.set_caption("Dash n Jump")
 clock = pygame.time.Clock()
 
 is_jump = False
+is_dash = False
 isCrash = False
 jumpCount = 7
+font_color = WHITE
 
 done = False
 pygame.mixer.music.load('MassiveEdge.wav')
@@ -39,17 +45,13 @@ pygame.mixer.music.play(-1)
 
 
 
-def draw_obstacle_n_check(x_coord):
-    pygame.draw.polygon(screen, ORANGE, [[x_coord, 400], [x_coord -25, 445], [x_coord + 25, 445]]) 
-    if not is_jump and abs(x_coord - 350) < coldis:
+def draw_obstacle_n_check(coord):
+    pygame.draw.polygon(screen, ORANGE, [[coord[0], coord[1]], [coord[0] -25, coord[1] + 100], [coord[0] + 25, coord[1] + 100]]) 
+    if not is_jump and abs(coord[0] - 350) < coldis and coord[1] == ob_l :
         return True
-    else:
-        return False
+    elif not is_dash and abs(coord[0] - 350) < coldis and coord[1] == ob_h:
+        return True
 
-def draw_obstacle_n_check2(x_coord):
-    pygame.draw.polygon(screen, ORANGE, [[x_coord, 375], [x_coord -100, -300], [x_coord + 100, -300]])
-    if not is_jump and abs(x_coord - 350) < coldis:
-        return True
     else:
         return False
 
@@ -73,22 +75,25 @@ while not done:
         isCrash = False
         background = BLACK
         pygame.mixer.music.play(-1)
-        obstacles = obstacles_org.copy()
+        obstacles = copy.deepcopy(obstacles_org)
+    
      
     if not(is_jump):        
         if keys[pygame.K_UP]:
             isJump = True
-        
-            
             is_jump = True
+        
         elif keys[pygame.K_DOWN]:
             height = 15
             wide =25
             y = 430
+            is_dash = True
         else: 
             height = 25
             wide = 20
             y = 420
+            is_dash = False
+
     else:
         if jumpCount >= -7:
             y -= (jumpCount * abs(jumpCount)) * 0.1
@@ -100,9 +105,9 @@ while not done:
     
     if not isCrash:
         for i in range(len(obstacles)):
-            obstacles[i] -= 1
-            if obstacles[i] < 0:
-                obstacles[i] += 1300
+            obstacles[i][0] -= 1
+            if obstacles[i][0] < 0:
+                obstacles[i][0] += 1300
         
     
     #Draw
@@ -111,10 +116,6 @@ while not done:
     pygame.draw.line(screen, WHITE, [0, 450], [3000, 450], 10)
     pygame.draw.rect(screen, RED, [x, y, wide, height], 5) 
     
-    
-
-#    pygame.draw.polygon(screen, ORANGE,[[o1, 400], [o1 -10, 445], [o1 +10, 445]])
-#    pygame.draw.polygon(screen, ORANGE,[[o2, 400], [o2 -10, 445], [o2 +10, 445]])
 
     for i in range(len(obstacles)):
         if draw_obstacle_n_check(obstacles[i]):
@@ -123,39 +124,27 @@ while not done:
             isCrash = True
 
 
-    font1 = pygame.font.SysFont('Calibri', 75, True, False)
-    font2 = pygame.font.SysFont('Calibri', 40, True, False)
-    font3 = pygame.font.SysFont('Calibri', 15, True, False)
-    font4 = pygame.font.SysFont('Calibri', 15, True, False)
-    font5 = pygame.font.SysFont('Calibri', 15, True, False)
-    font6 = pygame.font.SysFont('Calibri', 15, True, False)
-    font7 = pygame.font.SysFont('Calibri', 15, True, False)
-    font8 = pygame.font.SysFont('Calibri', 15, True, False)
+    font75 = pygame.font.SysFont('Calibri', 75, True, False)
+    font40 = pygame.font.SysFont('Calibri', 40, True, False)
+    font15 = pygame.font.SysFont('Calibri', 15, True, False)
 
-    text1 = font1.render("LET'S JUMP", True, WHITE)
-    text2 = font2.render(str(score) + " M", True, WHITE)
-    text3 = font3.render("Press Keyup = Jump", True, WHITE)
-    text4 = font4.render("Press Keydown = Dash", True, WHITE)
-    text5 = font5.render("Press Space = Play Again", True, WHITE)
-    text6 = font6.render("Press Keyup = Jump", True, BLACK)
-    text7 = font7.render("Press Keydown = Dash", True, BLACK)
-    text8 = font8.render("Press Space = Play Again", True, BLACK)
-    
+    text1 = font75.render("LET'S JUMP", True, WHITE)
+    text2 = font40.render(str(score) + " M", True, WHITE)
+    text3 = font15.render("Press Keyup = Jump", True, font_color)
+    text4 = font15.render("Press Keydown = Dash", True, font_color)
+    text5 = font15.render("Press Space = Play Again", True, font_color)    
  
     screen.blit(text1, [200, 100])
     screen.blit(text2, [60, 30])
+    
+    if score > 500:
+        font_color = BLACK
+    else:
+        font_color = WHITE
+        
     screen.blit(text3, [20, 70])
     screen.blit(text4, [20, 87])
     screen.blit(text5, [20, 104])
-
-    if score > 500:
-        screen.blit(text6, [20, 70])
-        screen.blit(text7, [20, 87])
-        screen.blit(text8, [20, 104])        
-    else: 
-        screen.blit(text3, [20, 70])
-        screen.blit(text4, [20, 87])
-        screen.blit(text5, [20, 104])
        
 
     
